88f306f0a421ec5a3d8b494b14a6ec750bd169bd
Aram Kocharyan <akarmenia@gmail.com>
2013-07-27 16:55:00 +1000

    Cleaner structor for attributes following inline CSS, parsed attributes.

--- src/Crayon.js
+++ src/Crayon.js
@@ -1,6 +1,6 @@
 define([
   'jquery', // src/jquery.js,
-   'defaults'
+  'defaults'
 ], function ($, defaults) {
 
   function Crayon(element, options) {
@@ -13,8 +13,25 @@ define([
   // Define plugin
   Crayon.prototype = {
 
+    elems: null,
+
     init: function () {
       console.error('init');
+      this.elems = this.query();
+      this.load(this.elems);
+    },
+
+    query: function () {
+      return $(this.options.selector, this.element);
+    },
+
+    load: function (elems) {
+      var me = this;
+      elems.each(function (i, pre) {
+        var atts = $(pre).attr(me.options.attrSelector);
+        var parsedAtts = me.options.attrParser(atts);
+        console.log('atts', atts, parsedAtts);
+      });
     }
 
   };
--- src/defaults.js
+++ src/defaults.js
@@ -1,7 +1,18 @@
 define([], function () {
   return {
 
-    propertyName: 'value'
+    selector: 'pre',
+    attrSelector: 'data-crayon',
+    // TODO must include global modifier, else infinite loop
+    reAttr: /(\w+\s*):(\s*[^;]+)/g,
+    attrParser: function (attStr) {
+      var match, atts = {};
+      while ((match = this.reAttr.exec(attStr)) != null) {
+        console.log('match', match);
+        atts[match[1]] = match[2];
+      }
+      return atts;
+    }
 
   };
 });
